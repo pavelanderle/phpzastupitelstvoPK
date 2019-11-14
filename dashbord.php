c4f8b7d1aadf8b294d45602ee3cb65f9def70b9a
pavelanderle <anderle@spseplzen.cz>
2019-11-14 10:06:14 +0100

    update dashbord

--- dashbord.php
+++ dashbord.php
@@ -8,7 +8,30 @@
     <?php
       include("funkce.php");
      ?>
+     <form class="" action="dashbord.php" method="post">
+      <p>
+        <label for="party">Zadej politickou stranu:</label>
+        <input type="text" name="party" >
+      </p>
 
-     
+      <p>
+        <label for="party">Zadej příjmení zastupitele:</label>
+        <input type="text" name="name" >
+      </p>
+       <input type="submit" value="Zobraz">
+     </form>
+
+     <?php
+        if(isset($_POST["party"]) && !empty($_POST["party"])){
+          $dataByParty=filterByParty(loadData("zastupitele.csv"),$_POST["party"]);
+          echo "<h2>Zastupitele PK zastupující ".$_POST["party"]."</h2>";
+          dataToTab($dataByParty);
+        }
+
+
+
+      ?>
+
+
   </body>
 </html>
